5399636fd806faf7b22909697adfa6722bdd429e
WillemBarendKruger <willem.kruger11@gmail.com>
2025-06-30 17:22:06 +0200

    Initial - Created file structure

---
+++ Ride-Along-Ride sharing system/Services/PaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ride_Along_Ride_sharing_system.Services
+{
+    internal class PaymentServices
+    {
+    }
+}
